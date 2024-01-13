1435cc31765d61459da10f044f1d7f9224cffba2
SquaredR98 <mail@ravi-ranjan.in>
2024-01-13 10:10:19 +0530

    Cleaned for the next app

--- src/components/ContentWrapper/index.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import './styles.scss';
-
-const ContentWrapper = ({ children }) => {
-	return <div className='content-wrapper'>{children}</div>;
-};
-
-export default ContentWrapper;
